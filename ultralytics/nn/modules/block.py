dfl softmax quint8 fix

ultralytics/nn/modules/block.py
@@ -2557,10 +2557,10 @@ class QDFL(nn.Module):
         """Apply the DFL module to input tensor and return transformed output."""
         b, _, a = x.shape  # batch, channels, anchors
         x = self.quant(x)
-        if (x.dtype == torch.float32):
-            return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)
-            # return self.conv(x.view(b, self.c1, 4, a).softmax(1)).view(b, 4, a)
-        else:
+        if (x.dtype == torch.quint8):
             x = self.conv(self.sm(x.view(b, 4, self.c1, a).transpose(2, 1))).view(b, 4, a)
             x = self.dequant(x)
             return x
+            # return self.conv(x.view(b, self.c1, 4, a).softmax(1)).view(b, 4, a)
+        else:
+            return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)