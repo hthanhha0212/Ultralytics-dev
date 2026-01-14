4f114ab88d038bb03199c099006204d154e036ad
hthanhha0212 <hthanh.ha.0212@gmail.com>
2026-01-14 11:27:37 +0700

    fbrt

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -26,6 +26,12 @@ __all__ = (
     "Concat",
     "QConcat",
     "RepConv",
+    "FCM",
+    "FCM_3",
+    "FCM_2",
+    "FCM_1",
+    "Pzconv",
+    "Down",
     "Index",
 )
 
@@ -653,6 +659,165 @@ class CBAM(nn.Module):
             (torch.Tensor): Attended output tensor.
         """
         return self.spatial_attention(self.channel_attention(x))
+
+
+class Channel(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dwconv = self.dconv = nn.Conv2d(dim, dim, 3, 1, 1, groups=dim)
+        self.apt = nn.AdaptiveAvgPool2d(1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x2 = self.dwconv(x)
+        x5 = self.apt(x2)
+        x6 = self.sigmoid(x5)
+        return x6
+
+
+class Spatial(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.conv1 = nn.Conv2d(dim, 1, 1, 1)
+        self.bn = nn.BatchNorm2d(1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x5 = self.bn(x1)
+        x6 = self.sigmoid(x5)
+        return x6
+
+
+class FCM_3(nn.Module):
+    def __init__(self, dim, dim_out):
+        super().__init__()
+        self.one = dim - dim // 4
+        self.two = dim // 4
+        self.conv1 = Conv(dim - dim // 4, dim - dim // 4, 3, 1, 1)
+        self.conv12 = Conv(dim - dim // 4, dim - dim // 4, 3, 1, 1)
+        self.conv123 = Conv(dim - dim // 4, dim, 1, 1)
+        self.conv2 = Conv(dim // 4, dim, 1, 1)
+        self.spatial = Spatial(dim)
+        self.channel = Channel(dim)
+
+    def forward(self, x):
+        x1, x2 = torch.split(x, [self.one, self.two], dim=1)
+        x3 = self.conv1(x1)
+        x3 = self.conv12(x3)
+        x3 = self.conv123(x3)
+        x4 = self.conv2(x2)
+        x33 = self.spatial(x4) * x3
+        x44 = self.channel(x3) * x4
+        x5 = x33 + x44
+        return x5
+
+
+class FCM_2(nn.Module):
+    def __init__(self, dim, dim_out):
+        super().__init__()
+        self.one = dim - dim // 4
+        self.two = dim // 4
+        self.conv1 = Conv(dim - dim // 4, dim - dim // 4, 3, 1, 1)
+        self.conv12 = Conv(dim - dim // 4, dim - dim // 4, 3, 1, 1)
+        self.conv123 = Conv(dim - dim // 4, dim, 1, 1)
+        self.conv2 = Conv(dim // 4, dim, 1, 1)
+        self.spatial = Spatial(dim)
+        self.channel = Channel(dim)
+
+    def forward(self, x):
+        x1, x2 = torch.split(x, [self.one, self.two], dim=1)
+        x3 = self.conv1(x1)
+        x3 = self.conv12(x3)
+        x3 = self.conv123(x3)
+        x4 = self.conv2(x2)
+        x33 = self.spatial(x4) * x3
+        x44 = self.channel(x3) * x4
+        x5 = x33 + x44
+        return x5
+
+
+class FCM_1(nn.Module):
+    def __init__(self, dim, dim_out):
+        super().__init__()
+        self.one = dim // 4
+        self.two = dim - dim // 4
+        self.conv1 = Conv(dim // 4, dim // 4, 3, 1, 1)
+        self.conv12 = Conv(dim // 4, dim // 4, 3, 1, 1)
+        self.conv123 = Conv(dim // 4, dim, 1, 1)
+        self.conv2 = Conv(dim - dim // 4, dim, 1, 1)
+        self.spatial = Spatial(dim)
+        self.channel = Channel(dim)
+
+    def forward(self, x):
+        x1, x2 = torch.split(x, [self.one, self.two], dim=1)
+        x3 = self.conv1(x1)
+        x3 = self.conv12(x3)
+        x3 = self.conv123(x3)
+        x4 = self.conv2(x2)
+        x33 = self.spatial(x4) * x3
+        x44 = self.channel(x3) * x4
+        x5 = x33 + x44
+        return x5
+
+
+class FCM(nn.Module):
+    def __init__(self, dim, dim_out):
+        super().__init__()
+        self.one = dim // 4
+        self.two = dim - dim // 4
+        self.conv1 = Conv(dim // 4, dim // 4, 3, 1, 1)
+        self.conv12 = Conv(dim // 4, dim // 4, 3, 1, 1)
+        self.conv123 = Conv(dim // 4, dim, 1, 1)
+        self.conv2 = Conv(dim - dim // 4, dim, 1, 1)
+        self.conv3 = Conv(dim, dim, 1, 1)
+        self.spatial = Spatial(dim)
+        self.channel = Channel(dim)
+
+    def forward(self, x):
+        x1, x2 = torch.split(x, [self.one, self.two], dim=1)
+        x3 = self.conv1(x1)
+        x3 = self.conv12(x3)
+        x3 = self.conv123(x3)
+        x4 = self.conv2(x2)
+        x33 = self.spatial(x4) * x3
+        x44 = self.channel(x3) * x4
+        x5 = x33 + x44
+        x5 = self.conv3(x5)
+        return x5
+
+
+class Pzconv(nn.Module):
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        super().__init__()
+        self.conv1 = nn.Conv2d(c1, c1, 3, 1, 1, groups=c1)
+        self.conv2 = Conv(c1, c1, k=1, s=1)
+        self.conv3 = nn.Conv2d(c1, c1, 5, 1, 2, groups=c1)
+        self.conv4 = Conv(c1, c1, 1, 1)
+        self.conv5 = nn.Conv2d(c1, c1, 7, 1, 3, groups=c1)
+        self.proj = Conv(c1, c2, 1, 1) if c2 != c1 else nn.Identity()
+        self.shortcut = Conv(c1, c2, 1, 1, act=False) if c2 != c1 else nn.Identity()
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x2 = self.conv2(x1)
+        x3 = self.conv3(x2)
+        x4 = self.conv4(x3)
+        x5 = self.conv5(x4)
+        x6 = self.proj(x5) + self.shortcut(x)
+        return x6
+
+
+class Down(nn.Module):
+    def __init__(self, dim, dim_out):
+        super().__init__()
+        self.conv2 = Conv(dim, dim, 3, 2, 1, g=dim // 2, act=False)
+        self.conv4 = Conv(dim, dim_out, 1, 1)
+
+    def forward(self, x):
+        x2 = self.conv2(x)
+        x2 = self.conv4(x2)
+        return x2
 
 
 class Concat(nn.Module):
